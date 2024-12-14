64d2172a5cb925bd5a1246f64798163405d3722a
rushanth01 <165635097+rushanth01@users.noreply.github.com>
2024-12-14 15:30:54 +0530

    Time: 1 ms (68.04%), Space: 42.6 MB (7.76%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -17,26 +17,44 @@ class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         Queue<TreeNode> q = new LinkedList<>();
         ArrayList<Integer> ans = new ArrayList<>();
-        if(root == null)
-        {
+        if(root == null){
             return ans;
         }
         q.add(root);
         while(q.size() > 0){
             int n = q.size();
             ArrayList<Integer> al = new ArrayList<>();
-            for(int i = 0;i< n;i++){
+            for(int i = 0 ;i  < n ; i++){
                 TreeNode temp = q.remove();
                 al.add(temp.val);
-                if(temp.left != null){
-                    q.add(temp.left);
-                }
-                if(temp.right != null){
-                    q.add(temp.right);
-                }
+                if(temp.left != null)q.add(temp.left);
+                if(temp.right != null)q.add(temp.right); 
             }
-            ans.add(al.get(al.size() -1 ));
+            ans.add(al.get(al.size()-1));
         }
         return ans;
+        // Queue<TreeNode> q = new LinkedList<>();
+        // ArrayList<Integer> ans = new ArrayList<>();
+        // if(root == null)
+        // {
+        //     return ans;
+        // }
+        // q.add(root);
+        // while(q.size() > 0){
+        //     int n = q.size();
+        //     ArrayList<Integer> al = new ArrayList<>();
+        //     for(int i = 0;i< n;i++){
+        //         TreeNode temp = q.remove();
+        //         al.add(temp.val);
+        //         if(temp.left != null){
+        //             q.add(temp.left);
+        //         }
+        //         if(temp.right != null){
+        //             q.add(temp.right);
+        //         }
+        //     }
+        //     ans.add(al.get(al.size() -1 ));
+        // }
+        // return ans;
     }
 }
